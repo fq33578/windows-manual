aa950ec2ff6e4febed6dcfacb00b97c2844f0978
CHEN,BO-HAN <fq33578@gmail.com>
2023-02-06 10:02:59 +0800

    Update and rename Open_NoPassword_FileShare to Open_NoPassword_FileShare.bat

--- Open_NoPassword_FileShare.bat
+++ Open_NoPassword_FileShare.bat
@@ -1,3 +1,3 @@
-::Turn off File Share Password Authority.
+::Turn off File Share No Password Authority.
 reg add HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\LanmanWorkstation /v AllowInsecureGuestAuth /t REG_DWORD /d 1 /f
 ::After CMD Run,Please Reboot Your PC.
