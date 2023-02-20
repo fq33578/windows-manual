b0515f2e6bf752c45f7bf3e5e4256c3c070ed36d
CHEN,BO-HAN <fq33578@gmail.com>
2023-02-21 00:14:57 +0800

    Update and rename ISO_Prepair.bat to ISO_Make.bat

---
+++ ISO_Make.bat
@@ -0,0 +1,4 @@
+::Backupdriver
+dism /online /export-driver /destination:D:/MyDrivers
+::Make New ISO Base Driver
+http://jasonyychiu.blogspot.com/2019/07/windows-how-to-add-driver-on-windows.html
--- ISO_Prepair.bat
+++
@@ -1,1 +0,0 @@
-dism /online /export-driver /destination:D:/MyDrivers
