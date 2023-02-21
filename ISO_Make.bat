86c66ae4d29e66f2f24b1f7029a294f7a0543c68
CHEN,BO-HAN <fq33578@gmail.com>
2023-02-21 09:39:06 +0800

    Update ISO_Make.bat

--- ISO_Make.bat
+++ ISO_Make.bat
@@ -1,13 +1,15 @@
 ::Backupdriver
-::1
+::1 Backup All Drivers
 dism /online /export-driver /destination:D:/MyDrivers
 ::Make New ISO Base Driver
 ::http://jasonyychiu.blogspot.com/2019/07/windows-how-to-add-driver-on-windows.html
-::2
+::2 Get Windows Version
 Dism /Get-WimInfo /WimFile:D:\Sources\install.wim 
-::3
+::3 Mount USB ISO
 Dism /Mount-Image /ImageFile:D:\Sources\install.wim /Index:3 /MountDir:C:\Mount 
-::4
+::4 ADD Drivers TO USB ISO
 Dism /Image:C:\Mount /Add-Driver /Driver:C:\Drivers /Recurse /ForceUnsigned
-::5
+::5 SAVE And Unmount USB ISO
 Dism /Unmount-Image /MountDir:C:\Mount /Commit 
+::6 Clean Trash Files
+Dism /Cleanup-Wim
