6cd69447e3e004f0545e0d056fa81dec04ce17aa
CHEN,BO-HAN <fq33578@gmail.com>
2023-02-25 18:20:10 +0800

    Update MS_Store_Enable.bat

--- MS_Store_Enable.bat
+++ MS_Store_Enable.bat
@@ -1,1 +1,4 @@
+::Enable MS Store
 Get-AppXPackage *WindowsStore* -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+::This Demo is ForWindows App Direcotry
+takeown /f "C:\Program Files\WindowsApps" /r
