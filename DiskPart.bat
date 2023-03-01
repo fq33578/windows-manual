311202991af1266415afe9ad4c6082ae5f08841f
CHEN,BO-HAN <fq33578@gmail.com>
2023-03-01 12:32:31 +0800

    Create DiskPart.bat

---
+++ DiskPart.bat
@@ -0,0 +1,15 @@
+::Show Disk or Volume List
+List Disk
+List Volume
+
+::Select Disk or Volume List
+Select Disk 1
+Select Volume 1
+
+::Clean Disk
+Clean
+
+::Convert Disk To GPT or MBR
+Select Disk 1
+Convert GPT
+Convert MBR
