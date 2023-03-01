45ea9fbc38b46c187c716f5aab87b1d52d4b6869
CHEN,BO-HAN <fq33578@gmail.com>
2023-03-01 10:41:40 +0800

    Update System_Move_Data.bat

--- System_Move_Data.bat
+++ System_Move_Data.bat
@@ -2,9 +2,19 @@
 ::https://www.upe.net/
 ::2. Boot  WinPE Open Diskgenius To Move System
 ::https://www.diskgenius.cn/download.php
-::3. Win+R Input "regedit"
+
+:::If Black Screen Only Mouse Cursor Please Try:::
+::3. Press Win+R Input "regedit"
 ::4. Press "HKEY_LOCAL_MOCHINE" 
 ::5. Press "File->LOAD LOAD Register Zone"
 ::6. Load Your Target System Disk "SYSTEM" File ex:"F:\Windows\System32\config\SYSTEM"
 ::7. Find "MountedDevices"
 ::8. Remove All Mount Disk
+
+:::If Blue Screen Code 0x000000e Please Try:::
+::9. Copy Target Disk GUID From Disk Genius
+::10. Open CMD From Admin
+::11. If Target Disk is F, Input "bcdboot F:\windows /l zh-tw"
+::12. If Target Disk is F, Input "bcdboot F:\windows /m {TARGET-DISK-ID}"
+::13. If Target Disk is F, BIOS Select Default Boot F Disk
+::14.Reboot PC.
