233b0421113d6cafba7c7780d159c2163940babf
CHEN,BO-HAN <fq33578@gmail.com>
2023-03-01 10:04:30 +0800

    Update and rename system_move_data.bat to System_Move_Data.bat

---
+++ System_Move_Data.bat
@@ -0,0 +1,10 @@
+::1. Make EASYU WinPE To Boot USB 
+::https://www.upe.net/
+::2. Boot  WinPE Open Diskgenius To Move System
+::https://www.diskgenius.cn/download.php
+::3. Win+R Input "regedit"
+::4. Press "HKEY_LOCAL_MOCHINE" 
+::5. Press "File->LOAD LOAD Register Zone"
+::6. Load Your Target System Disk "SYSTEM" File ex:"F:\Windows\System32\config\SYSTEM"
+::7. Find "MountedDevices"
+::8. Remove All Mount Disk
--- system_move_data.bat
+++
@@ -1,7 +0,0 @@
-::1. Use Disk Genuis Move System
-::2. Win+R Input "regedit"
-::3. Press "HKEY_LOCAL_MOCHINE" 
-::4. Press "File->LOAD LOAD Register Zone"
-::5. Load Your Target System Disk "SYSTEM" File ex:"F:\Windows\System32\config\SYSTEM"
-::6. Find "MountedDevices"
-::7. Remove All Mount Disk
