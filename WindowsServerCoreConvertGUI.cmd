4c179f78b83c0e045857e0e7a5a99f51a9779ad2
CHEN,BO-HAN <fq33578@gmail.com>
2023-04-11 09:12:25 +0800

    Update WindowsServerCoreConvertGUI.cmd

--- WindowsServerCoreConvertGUI.cmd
+++ WindowsServerCoreConvertGUI.cmd
@@ -2,4 +2,4 @@
 Get-WindowsImage -ImagePath D:\Sources\Install.wim
 
 ::2. install GUI From Local Image & Restart Server.
-Install-WindowsFeature Server-Gui-Mgnt-Infra.Server-Gui-Shell -Restart -Source wip:D: 1so esLinstall.wiu:2
+Install-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell -Restart -Source wim:D:\Sources\Install.wim
